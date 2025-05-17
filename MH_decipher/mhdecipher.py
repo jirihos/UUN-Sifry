84f23487d8b8baf8af58b5c2db78a6259947cd49
Martin Nebehay <martinnebe456@gmail.com>
2025-05-17 21:52:27 +0200

    transition_matrix

--- MH_decipher/mhdecipher.py
+++ MH_decipher/mhdecipher.py
@@ -14,8 +14,25 @@ def get_bigrams(text: str) -> list[str]:
         bigrams_list.append(bigram)
     return bigrams_list
 
-def transition_matrix(bigrams: list[str]) -> np.ndarray:
-    pass
+def transition_matrix(bigrams: list[str]) -> pd.DataFrame:
+    n = len(alphabet)
+    TM = pd.DataFrame(
+        np.zeros((n, n), dtype=int),
+        index=alphabet,
+        columns=alphabet
+    )
+
+    for bigram in bigrams:
+        c1 = bigram[0]
+        c2 = bigram[1]
+        if c1 in alphabet and c2 in alphabet:
+            i = alphabet.index(c1)
+            j = alphabet.index(c2)
+            TM.iat[i, j] += 1
+
+    TM.replace(0, 1, inplace=True)
+
+    return TM
 
 def plausibility(text: str, TM_ref: np.ndarray):
     pass
